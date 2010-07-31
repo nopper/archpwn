a359c3501b413752869963e7b345941906c298a9
nopper <nopper@archpwn.org>
2010-07-31 12:48:06 +0200

    Implemented 02-shortcuts.sh iso hook

--- scripts/hooks/02-shortcuts.sh
+++ scripts/hooks/02-shortcuts.sh
@@ -3,5 +3,12 @@
 # TODO: check package list
 
 run_hook() {
-    info "foo"
+    info "Building shortcuts (this may take few minutes) ..."
+    desktop-gen -s 1 -o "$IMGROOT/$CDNAME"/modules/shortcuts.lzm "$PACMAN_PKG"
+
+    if [ $? -ne 0 ]; then
+        warn "Something went wrong!"
+    else
+        info "Shortcuts created succefully."
+    fi
 }
